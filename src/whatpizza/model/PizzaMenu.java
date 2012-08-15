89d196c015f794fc39d9717a5559b479edb7c86a
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-08-15 19:59:53 -0300

    Pizza Menu model and Tests

--- src/whatpizza/model/PizzaMenu.java
+++ src/whatpizza/model/PizzaMenu.java
@@ -1,5 +1,26 @@
 package whatpizza.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PizzaMenu {
+
+	private List<String> pizzas;
+
+	public PizzaMenu() {
+		pizzas = new ArrayList<String>();
+	}
+	
+	public List<String> all() {
+		return pizzas;
+	}
+
+	public void add(String tastePizza) {
+		pizzas.add(tastePizza);
+	}
+
+	public void remove(String tastePizza) {
+		pizzas.remove(tastePizza);
+	}
 
 }
--- src/whatpizza/test/PizzaMenuTest.java
+++ src/whatpizza/test/PizzaMenuTest.java
@@ -5,9 +5,32 @@ import junit.framework.TestCase;
 
 public class PizzaMenuTest extends TestCase {
 	
+	private PizzaMenu menu;
+
 	public void testCreateMenu() {
-		PizzaMenu menu = new PizzaMenu();
-		assertNotNull(menu);
+		assertNotNull("should not be null",menu);
+	}
+	
+	public void testGetAllPizza() {
+		assertEquals("should return some List","[]", menu.all().toString());
+		
+	}
+	
+	public void testAddPizza() {
+		menu.add("Pepperoni");
+		assertFalse(menu.all().isEmpty());
+		assertTrue("should contains a Pepperoni Pizza", menu.all().contains("Pepperoni"));
+	}
+	
+	public void testRemovePizza() {
+
+		menu.add("Marguerita");
+		menu.remove("Marguerita");
+		assertTrue(menu.all().isEmpty());
+		assertFalse("should not contains Marguerita Pizza",menu.all().contains("Marguerita"));
 	}
 
+	public void setUp() {
+		menu = new PizzaMenu();
+	}
 }
