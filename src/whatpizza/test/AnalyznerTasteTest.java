f5fa9ba424b02f0a65ef798cc9e4de3155d10927
Lucas Lima de Souza <lucaslima4p@gmail.com>
2012-08-16 10:18:12 -0300

    similar taste found

--- src/whatpizza/test/AnalyznerTasteTest.java
+++ src/whatpizza/test/AnalyznerTasteTest.java
@@ -32,7 +32,7 @@ public class AnalyznerTasteTest extends TestCase {
 	
 	public void testParticularTotalNotesPizza() {
 		int[] arrayNotes = {5,5,4};
-		Map<String,	Integer>  highNotes = buildHashhNotes(arrayNotes);
+		Map<String,Integer>  highNotes = buildHashhNotes(arrayNotes);
 		Person person = mock(Person.class);
 		when(person.pizzaNotes()).thenReturn(highNotes);
 		assertEquals(14, analyzer.particularTotalNotesPizza(person));
@@ -44,21 +44,21 @@ public class AnalyznerTasteTest extends TestCase {
 		analyzer.addPerson(similarTastePerson);
 		analyzer.addPerson(wrongTastePerson);
 		Person personToCompare = buildPersonToCompare();
-		assertEquals(similarTastePerson.toString(),analyzer.whoHaveSimilarPizzaTaste(personToCompare));
+		assertEquals(similarTastePerson.toString(),analyzer.whoHaveSimilarPizzaTaste(personToCompare).toString());
 	}
 
 	private Person buildPersonToCompare() {
 		Person personToCompare = mock(Person.class);
-		int[] arrayHighNotes = {5,5,4};
-		Map<String,	Integer> highNotes = buildHashhNotes(arrayHighNotes);
+		int[] arrayHighNotes = {3,3,1};
+		Map<String,Integer> highNotes = buildHashhNotes(arrayHighNotes);
 		when(personToCompare.pizzaNotes()).thenReturn(highNotes);
 		return personToCompare;
 	}
 
 	private Person buildWrongTastePerson() {
 		Person wrongTastePerson = mock(Person.class);
-		int[] arraySlowNotes = {3,2,5};
-		Map<String, Integer> slowNotes = buildHashhNotes(arraySlowNotes);
+		int[] arraySlowNotes = {5,4,5};
+		Map<String,Integer> slowNotes = buildHashhNotes(arraySlowNotes);
 		when(wrongTastePerson.pizzaNotes()).thenReturn(slowNotes);
 		when(wrongTastePerson.toString()).thenReturn("Wrong");
 		return wrongTastePerson;
@@ -66,7 +66,7 @@ public class AnalyznerTasteTest extends TestCase {
 
 	private Person buildSimilarTastePerson() {
 		Person similarTastePerson = mock(Person.class);
-		int[] arrayMiddleNotes = {5,4,4};
+		int[] arrayMiddleNotes = {5,5,1};
 		Map<String, Integer> middleNotes = buildHashhNotes(arrayMiddleNotes);
 		when(similarTastePerson.pizzaNotes()).thenReturn(middleNotes);
 		when(similarTastePerson.toString()).thenReturn("João");
--- src/whatpizza/util/AnalyzerTaste.java
+++ src/whatpizza/util/AnalyzerTaste.java
@@ -1,6 +1,7 @@
 package whatpizza.util;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import whatpizza.model.Person;
@@ -21,16 +22,33 @@ public class AnalyzerTaste {
 		people.add(person);
 	}
 
-	public String whoHaveSimilarPizzaTaste(Person personToCompare) {
-		return "João";
+	public Person whoHaveSimilarPizzaTaste(Person personToCompare) {
+		int tasteTocompare = particularTotalNotesPizza(personToCompare);
+		return  findSimilarPerson(tasteTocompare);
+	}
+
+	private Person findSimilarPerson(int tasteTocompare) {
+		Person similarPerson = null;
+		int similarTaste = tasteTocompare;
+		for (Person person : people) {
+			int clonessesTaste = calculateCloseTaste(tasteTocompare, person);
+			if( clonessesTaste > 0 && clonessesTaste <= similarTaste){
+				similarTaste = clonessesTaste;
+				similarPerson = person;
+			}}return similarPerson;
+	}
+
+	private int calculateCloseTaste(int tasteTocompare, Person person) {
+		int clonessesTaste = (particularTotalNotesPizza(person) - tasteTocompare);
+		if(clonessesTaste < 0) return clonessesTaste* -1;
+		return clonessesTaste;
 	}
 
 	public int particularTotalNotesPizza(Person person) {
 		int total =0;
 		for (Integer note : person.pizzaNotes().values()) {
 			 total += note;
-		}
-		return total;
+		}return total;
 	}
 
 }
